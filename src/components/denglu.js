fb037238df7235dbb3760906bd1aa4123d29cb64
sunfuhao <sfhgr25852@163.com>
2018-05-08 12:00:19 +0800

    zhuce

--- src/components/denglu.js
+++ src/components/denglu.js
@@ -1,49 +1,86 @@
 import React from 'react'
-import { Form, Icon, Input, Button, Checkbox ,Select,Radio} from 'antd';
+import { Form, Icon, Input, Button, Checkbox ,Select,Radio,Row,Col} from 'antd';
 const FormItem = Form.Item
 const Option = Select.Option
 const RadioGroup = Radio.Group
 
-class denglu extends React.Component {  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
+class denglu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            yanzheng: "获取验证码",
+            kaishidaojishi:true,
+            upsize :'small',
+        }
+    }
+    yanzheng = ()=>{
+        if(this.state.yanzheng){
+            if(this.state.kaishidaojishi){
+                this.setState({kaishidaojishi:false})  
+                let times = 60;
+                let timer = setInterval(()=>{
+                    this.setState({yanzheng:times})  
+                    times-=1
+                    if(times==0){
+                        this.setState({yanzheng:"获取验证码"})
+                        clearInterval(timer)
+                        this.setState({kaishidaojishi:true})  
+                    }  
+                },1000)
+            }else{
+                return
+            }
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+   
+    }
   render() {
+    const formItemLayout = {
+        labelCol: { span: 8,offset:0 },
+        wrapperCol: { span: 8 },
+    };
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-        {getFieldDecorator('userName', {
-        rules: [{ required: true, message: 'Please input your username!' }],
+        <div >
+      <Form onSubmit={this.handleSubmit} className="login-form loginbox">
+        <FormItem {...formItemLayout} label="手机号">
+        {getFieldDecorator('iphone', {
+        id:"iphone",
+        rules: [{ required: true, message: '请输入正确的11位手机号!',max:11,min:11}],
         })(
-        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请填写手机号" />
         )}
         </FormItem>
-        <FormItem>
-        {getFieldDecorator('password', {
-        rules: [{ required: true, message: 'Please input your Password!' }],
-        })(
-        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-        )}
+        <FormItem
+          {...formItemLayout}
+          label="验证码"
+        >
+          <Row gutter={10}>
+            <Col span={12}>
+                {getFieldDecorator('yanzheng', {
+                id:"yanzheng",
+                rules: [{ required: true,max:4,min:4,message: '请输入4位验证码!'}],
+                })(
+                <Input placeholder="请填写验证码"/>
+                )}
+            </Col>
+            <Col span={12}>
+              <Button onClick={this.yanzheng}>{this.state.yanzheng}</Button>
+            </Col>
+          </Row>
         </FormItem>
-        <FormItem>
-        {getFieldDecorator('remember', {
-        valuePropName: 'checked',
-        initialValue: true,
-        })(
-        <Checkbox>Remember me</Checkbox>
-        )}
-        <a className="login-form-forgot" href="">Forgot password</a>
         <Button type="primary" htmlType="submit" className="login-form-button">
         Log in
         </Button>
-        Or <a href="">register now!</a>
-        </FormItem>
       </Form>
+      </div>
     );
   }
 }
--- src/components/myabout.js
+++ src/components/myabout.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Layout } from 'antd';
-import { Card,Row, Col } from 'antd';
+import { Card,Row, Col,Modal} from 'antd';
 import { message,Upload,Avatar,Button,Icon,notification } from 'antd';
+import UpJianli from './upjianli'
 
 // const { Header} = Layout;
 import { Link, hashHistory, IndexRoute, Redirect, IndexLink} from 'react-router'
@@ -15,7 +16,7 @@ export default class MyAbout extends React.Component {
         this.state = {
             user: {},
             myjianli:{},
-            upsize :'small',
+            visible:false
         }
     }
     openNotification = (title,desc) => {
@@ -50,7 +51,14 @@ export default class MyAbout extends React.Component {
     componentDidMount() {
          this.fetchFn()
     }
-
+    upload =(e)=>{
+        this.setState({
+            visible:true
+        })
+    }
+    hideModal = () => {
+        this.setState({ visible: false })
+    }
 render(){
         const props = {
           name: 'file',
@@ -74,7 +82,7 @@ render(){
         if(Object.keys(this.state.myjianli).length == 0){
             _jianli.push(<div className="myjianli">{this.state.myjianli.title}</div>)
         }else{
-             _jianli.push(<Upload {...props}><Button><Icon type="upload" /> 上传简历</Button></Upload>)
+             _jianli.push(<Button size="large" onClick={this.upload.bind(this)}>上传简历</Button>)
         }
 
         return(
@@ -83,6 +91,9 @@ render(){
                     <Avatar shape="square" url={this.state.user.user_icon} size="large" icon="user" />
 
                 </div>
+                <Modal title="上传简历" visible={this.state.visible} onOk={this.hideModal} onCancel={this.hideModal} footer={null}>
+                    <UpJianli />
+                </Modal>
                 <div>我的简历：{_jianli}</div>
                 <div> 我的金钱：<Button size="small">{this.state.user.user_money}</Button> <Icon type="unlock" />（可提现）</div>
                 <div> 我的金钱：<Button size="small" type="dashed" onClick={this.openNotification.bind(this,'不可以提现','因为这并不是您充值的部分～')}>{this.state.user.bad_money}</Button> <Icon type="lock" />（不可提现）</div>
